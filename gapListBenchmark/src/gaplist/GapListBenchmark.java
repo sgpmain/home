6dcd4b50f3d100c69b3c4f7e9e3e8a729378b2be
webos <webos@localhost>
2014-06-02 16:36:08 +0930

    testing performance of diff implementations of list interface with jmh

---
+++ gapListBenchmark/src/gaplist/GapListBenchmark.java
@@ -0,0 +1,25 @@
+package gaplist;
+import org.openjdk.jmh.annotations.*;
+import static java.util.concurrent.TimeUnit.*;
+
+//@BenchmarkMode(Mode.AverageTime)
+//@OutputTimeUnit(NANOSECONDS)
+//@Warmup(iterations = 10, time = 1, timeUnit = SECONDS)
+//@Measurement(iterations = 10)
+//@Fork(5)
+//@State(Scope.Benchmark)
+
+
+
+public class GapListBenchmark {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+     //@GenerateMicroBenchmark
+
+
+}
